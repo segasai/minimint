b4a3fb469f94e45e13ee09582beea14f9c42d801
Sergey E. Koposov <skoposov@ed.ac.uk>
2023-12-27 09:34:48 +0000

    fix hanging

--- py/minimint/mist_interpolator.py
+++ py/minimint/mist_interpolator.py
@@ -175,7 +175,8 @@ def download_and_prepare(filters=[
                    f'7z x {fname_out1}')
         else:
             cmd = f'cd {pref}; tar xfJ {fname_out}'
-        ret = subprocess.run(cmd, capture_output=True, shell=True, timeout=60)
+        #ret = subprocess.run(cmd, capture_output=True, shell=True, timeout=60)
+        ret = subprocess.run(cmd, shell=True, timeout=60)
         if ret.returncode != 0:
             raise RuntimeError('Failed to untar the files' +
                                ret.stdout.decode() + ret.stderr.decode())
